8661a9b46f7ca9575b2001feb97c649040de940d
Khánh Trần <23014765st.phenikaa-uni.edu.vn>
2026-05-23 16:06:12 +0700

    hihi

--- config/database.php
+++ config/database.php
@@ -59,9 +59,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+           'options' => (env('MYSQL_ATTR_SSL_CA') === 'true' || env('MYSQL_ATTR_SSL_CA')) ? array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => true,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    ]) : [],
         ],
 
         'mariadb' => [
